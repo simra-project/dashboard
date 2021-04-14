dc713eebe91cb856ff2138c108a4aae0248587c8
MoeweX <Jonathan@Hasenburg.de>
2021-04-14 15:04:22 +0200

    Allow for longer versions

--- data-parser/src/main/kotlin/util/Extensions.kt
+++ data-parser/src/main/kotlin/util/Extensions.kt
@@ -2,9 +2,9 @@ package util
 
 /**
  * Returns an int pair based on this list.
- * Must comprise only two elements, non digit characters are dropped.
+ * Must comprise at least two elements, non digit characters are dropped and additional elements
  */
 fun List<String>.toIntPair(): Pair<Int, Int> {
-    require(this.size == 2) {"Given list does not have size 2, cannot create Int-Pair."}
+    require(this.size < 2) {"Given list does not have size 2, cannot create Int-Pair."}
     return Pair(this[0].filter { it.isDigit() }.toInt(), this[1].toInt())
 }
