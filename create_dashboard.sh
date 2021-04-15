054b53c51230b249668dd65ead78fe35b9918c3d
mcc_admin <admin_mcc@vm2.mcc.tu-berlin.de>
2021-04-15 12:34:15 +0200

    fix create_dashboard.sh

--- create_dashboard.sh
+++ create_dashboard.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-(cd data-parser && java -jar data-parser-1.0-fat.jar -s sdb/SimRa/SimRa)
+(cd data-parser && java -jar data-parser-1.0-fat.jar -s /sdb/SimRa/SimRa)
 (cd simra-project.github.io && ./commit.sh)
