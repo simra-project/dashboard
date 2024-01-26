4b52a22e57925f858a8e715df31031206a87e5cb
selphiron <a.karakaya@tu-berlin.de>
2024-01-26 09:08:15 +0100

    Update create_dashboard.sh

--- create_dashboard.sh
+++ create_dashboard.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-(cd data-parser && java -jar data-parser-1.1-fat.jar -s /sdb/SimRa/Regions)
-(cd simra-project.github.io && ./commit.sh)
+DATAPARSERDIR=/sdb/SimRa/dashboard/data-parser
+WEBSITEDIR=/sdb/SimRa/dashboard/simra-project.github.io
+(cd $DATAPARSERDIR && java -jar data-parser-1.1-fat.jar -s /sdb/SimRa/Regions -o true)
+(cd $WEBSITEDIR && ./commit.sh)
